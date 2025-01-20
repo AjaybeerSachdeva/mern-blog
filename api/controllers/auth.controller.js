92aef019b7fb01f79cccb4f1ca5fa9a0ddbda0b1
AjaybeerSachdeva <134932767+AjaybeerSachdeva@users.noreply.github.com>
2025-01-20 16:27:52 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
@@ -18,4 +19,32 @@ export const signup=async(req,res,next)=>{
     {
         next(error);
     }
+}
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    if(!email || !password || email==='' || password==='')
+    {
+        return next(errorHandler(400,"All fields are required"));
+    }
+    try{
+    const existingUser=await User.findOne({email});
+    if(!existingUser)
+    {
+        return next(errorHandler(404,"User does not exist"));
+    }
+    const validPassword=bcrypt.compareSync(password, existingUser.password); 
+    if(validPassword===false)
+    {
+        return next(errorHandler(400,"Wrong credentials!"));
+    }
+    const token=jwt.sign({userId:existingUser._id},process.env.JWT_SECRET_KEY);
+    const {password:pass,...rest}=existingUser._doc;
+    res.status(200).cookie('access_token',token,{httpOnly:true}).json({message:"User signed in successfully",details:rest});
+    }
+    catch(error)
+    { 
+        next(error);
+    }
+
 }
