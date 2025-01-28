561804d6e57db532a1b3b6254669a87fdfce5380
AjaybeerSachdeva <134932767+AjaybeerSachdeva@users.noreply.github.com>
2025-01-29 01:22:36 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,13 +3,14 @@ import { useState } from 'react';
 import { FaGoogle } from "react-icons/fa";
 import { Link, useNavigate } from 'react-router-dom';
 import { HiInformationCircle } from "react-icons/hi";
-
+import { useSelector, useDispatch } from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/UserSlice';
 
 export default function SignIn() {
   const navigate=useNavigate();
+  const dispatch=useDispatch();
+  const {loading,error:errorMessage}=useSelector((state)=>state.user);
   const [formData,setFormData]=useState({});
-  const [errorMessage,setErrorMessage]=useState(null);
-  const [loading,setLoading]=useState(false);
   const handleChange=(e)=>{
     setFormData(
       {
@@ -23,11 +24,10 @@ export default function SignIn() {
     e.preventDefault(); 
     if(!formData.email || !formData.password)
     {
-      return setErrorMessage("Please fill out all the fields.");
+      return dispatch(signInFailure("Please fill out all the fields."));
     }
     try{
-      setLoading(true);
-      setErrorMessage(null);
+    dispatch(signInStart()); 
     const res=await fetch('/api/auth/signin',{
       method:"POST",
       body:JSON.stringify(formData),
@@ -38,19 +38,17 @@ export default function SignIn() {
     const data=await res.json();
     if(data.success===false)
     {
-      setLoading(false);
-      return setErrorMessage(data.message);
+      return dispatch(signInFailure(data.message));
     }
-    setLoading(false);
     if(res.ok)
     {
+      dispatch(signInSuccess(data));
       navigate('/');
     }
   }
   catch(error)
   {
-    setErrorMessage(error.message);
-    setLoading(false);
+    dispatch(signInFailure(error.message)); 
   }
   }
   return (
