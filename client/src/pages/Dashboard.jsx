1c3d6a07e8c4789b898f81293ce7a5fb9c1437f4
AjaybeerSachdeva <134932767+AjaybeerSachdeva@users.noreply.github.com>
2025-06-28 08:23:43 +0530

    complete profile page UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
-import { DashProfile } from '../components/DashProfile';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
   const location=useLocation();
@@ -21,10 +21,8 @@ export default function Dashboard() {
         {/* sidebar */}
         <DashSidebar/>
       </div>
-      <div className=''> 
         {/* profile */}
         {tab==='profile' && <DashProfile/>}
-      </div>
     </div>
   )
 }
